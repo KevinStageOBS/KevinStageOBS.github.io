5c937c79cef90ec2201d2930a1711fec6c455ade
KevinStageOBS <kevin.despoulains@orange.com>
2020-06-10 15:58:19 +0200

    publish website flutter_web2

---
+++ projects/flutter_web2/root-service-workers.js
@@ -0,0 +1,5 @@
+/* Bump the service worker version in this comment to make sure changes to imported scripts are reflected on the client/browser.
+SERVICE WORKER VERSION = 6
+*/ 
+
+//importScripts('my_service_worker.js', 'flutter_service_worker.js');
